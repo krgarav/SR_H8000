3cfe7b96f6b0ca9fe27007b93c89dffbfe5df394
krgarav <krgarav@gmail.com>
2024-08-08 18:15:31 +0530

    Added Moderator Queue list

--- src/moderator.js
+++ src/moderator.js
@@ -15,45 +15,50 @@ import Booklet32Page from "views/Booklet32Page";
 import Booklet24Page from "views/Booklet24Page";
 import ImageGrid from "views/Imagegrid";
 import FolderStructure from "views/FolderStructure";
+import ModeratorJobQueue from "views/ModeratorJobQueue";
 
 var moderatorRoute = [
-    {
-        path: "/index",
-        name: "Dashboard",
-        icon: "ni ni-tv-2 text-primary",
-        component: <Index />,
-        layout: "/moderator",
-    },
-    {
-        path: "/jobs",
-        name: "Job Management",
-        icon: "ni ni-briefcase-24 text-yellow",
-        component: <Jobs />,
-        layout: "/moderator",
-    },
-    {
-        path: "/32-page-booklet",
-        name: "Scan Template",
-        icon: "ni ni-books text-success",
-        component: <Booklet32Page />,
-        layout: "/moderator",
-    },
-    // {
-    //     path: "/setting",
-    //     name: "Settings",
-    //     icon: "ni ni-settings-gear-65 text-primary",
-    //     component: <FolderPickerDirectory />,
-    //     layout: "/admin",
-    // },
-    {
-        path: "/server-folder",
-        name: "Folder data",
-        icon: "ni ni-settings-gear-65 text-primary",
-        component: <FolderStructure />,
-        layout: "/moderator",
-    },
-
-
-
+  {
+    path: "/index",
+    name: "Dashboard",
+    icon: "ni ni-tv-2 text-primary",
+    component: <Index />,
+    layout: "/moderator",
+  },
+  {
+    path: "/jobs",
+    name: "Job Management",
+    icon: "ni ni-briefcase-24 text-yellow",
+    component: <Jobs />,
+    layout: "/moderator",
+  },
+  // {
+  //     path: "/32-page-booklet",
+  //     name: "Scan Template",
+  //     icon: "ni ni-books text-success",
+  //     component: <Booklet32Page />,
+  //     layout: "/moderator",
+  // },
+  // {
+  //     path: "/setting",
+  //     name: "Settings",
+  //     icon: "ni ni-settings-gear-65 text-primary",
+  //     component: <FolderPickerDirectory />,
+  //     layout: "/admin",
+  // },
+  {
+    path: "/icons",
+    name: "Admin Job Queue",
+    icon: "ni ni-money-coins text-yellow",
+    component: <ModeratorJobQueue />,
+    layout: "/moderator",
+  },
+  {
+    path: "/server-folder",
+    name: "Folder data",
+    icon: "ni ni-settings-gear-65 text-primary",
+    component: <FolderStructure />,
+    layout: "/moderator",
+  },
 ];
 export default moderatorRoute;
